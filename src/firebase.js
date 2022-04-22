ba01ce3f324ec30f6aca8c25095ec632e28f66db
kunlefash <kunlefashmayop@gmail.com>
2022-04-22 19:47:11 -0300

    master

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC103lpZqljGgGtjTVPKffwig8CV1uJ9r8",
+    authDomain: "ecommerce-website-b4d91.firebaseapp.com",
+    projectId: "ecommerce-website-b4d91",
+    storageBucket: "ecommerce-website-b4d91.appspot.com",
+    messagingSenderId: "474503556368",
+    appId: "1:474503556368:web:619dc4935c92ecf06a0995",
+    measurementId: "G-8LM2VP8W56"
+  };
